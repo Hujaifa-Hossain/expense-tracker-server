92eaabd0d419a07dab09494100e4e07914afd9cd
Hujaifa21 <21hujaifa@gmail.com>
2022-11-01 02:25:23 +0600

    database connected

--- database/connection.js
+++ database/connection.js
@@ -1,1 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+const connection = mongoose
+  .connect(process.env.ATLAS_URI)
+  .then((db) => {
+    console.log("db connected");
+    return db;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+module.exports = connection;
--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const express = require("express");
-const route = require('./routes/route')
 const cors = require("cors");
-require("dotenv").config();
+const route = require("./routes/route");
+require("dotenv").config({path: './config.env'});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
-// using routes 
 app.use(route);
 
-// listen to the http server 
-app.listen(PORT, () => {
-  console.log(`Server is walking on port: ${PORT}`);
+const conn = require("./database/connection");
+
+conn.then(db => {
+  if(!db) return process.exit(1);
+
+  // listen to the http server 
+  app.listen(PORT, () => {
+      console.log(`Server is running on port: http://localhost:${PORT}`)
+  })
+
+  app.on('error', err => console.log(`Failed To Connect with HTTP Server : ${err}`));
+  // error in mondb connection
+}).catch(error => {
+  console.log(`Connection Failed...! ${error}`);
 });
